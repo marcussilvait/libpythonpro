49f94120b5e778dd1864f0c658ab0daa7b8c33ed
marvinsilva <vnc_vinicius92@hotmail.com>
2020-03-02 18:01:19 -0300

    Criado test spam e enviador de email

---
+++ libpythonpro/tests/test_spam/test_enviador_de_email.py
@@ -0,0 +1,38 @@
+import pytest
+
+from libpythonpro.spam.enviador_de_email import Enviador, EmailInvalido
+
+
+def test_criar_enviador_de_email():
+    enviador = Enviador()
+    assert enviador is not None
+
+
+@pytest.mark.parametrize(
+    'destinatario',
+    ['vnc_vinicius92@hotmail.com', 'foo@bar.com.br']
+)
+def test_remetente(destinatario):
+    enviador = Enviador()
+    resultado = enviador.enviar(
+        destinatario,
+        'vnctm10@gmail.com',
+        'Cursos Python Pro',
+        'Terceira turma Python Pro Bruno Rocha'
+    )
+    assert destinatario in resultado
+
+
+@pytest.mark.parametrize(
+    'remetente',
+    ['', 'vnc_vinicius92']
+)
+def test_remetente(remetente):
+    enviador = Enviador()
+    with pytest.raises(EmailInvalido):
+        resultado = enviador.enviar(
+            remetente,
+            'vnctm10@gmail.com',
+            'Cursos Python Pro',
+            'Terceira turma Python Pro Bruno Rocha'
+        )
