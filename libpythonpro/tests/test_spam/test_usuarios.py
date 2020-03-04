0c2940f827d3d4decaab568db3d25a662feb989b
marvinsilva <vnc_vinicius92@hotmail.com>
2020-03-04 08:55:21 -0300

    Criado test 'test_qde_de_spam' - Produção de código testável

--- libpythonpro/tests/test_spam/test_usuarios.py
+++ libpythonpro/tests/test_spam/test_usuarios.py
@@ -2,13 +2,16 @@ from libpythonpro.spam.modelos import Usuario
 
 
 def test_salvar_usuario(sessao):
-    usuario = Usuario(nome='Marcus')
+    usuario = Usuario(nome='Marcus', email='vnc_vinicius92@hotmail.com')
     sessao.salvar(usuario)
     assert isinstance(usuario.id, int)
 
 
 def test_listar_usuarios(sessao):
-    usuarios = [Usuario(nome='Marcus'), Usuario(nome='Luciano')]
+    usuarios = [
+        Usuario(nome='Marcus', email='vnc_vinicius92@hotmail.com'),
+        Usuario(nome='Luciano', email='vnc_vinicius92@hotmail.com')
+    ]
     for usuario in usuarios:
         sessao.salvar(usuario)
     assert usuarios == sessao.listar()
